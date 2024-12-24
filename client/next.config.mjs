e6dd2f286680f06972593c6e333ebea79cd1723a
kimtaekyong <lkss98@gmail.com>
2024-12-24 09:29:11 +0900

    buildCSS

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -18,6 +18,13 @@ const nextConfig = {
       }
       return config;
     },
+    content: [
+      "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+      "./Components/**/*.{js,ts,jsx,tsx,mdx}",
+      "./app/**/*.{js,ts,jsx,tsx,mdx}",
+      "./node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+      "./node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
+    ],
   },
   async rewrites() {
     return process.env.NODE_ENV === "development"
